4e7ae2bec16e561e3b1ab150a03d338018000a28
Curtis Rueden <ctrueden@wisc.edu>
2017-07-05 15:05:55 -0500

    Tensors: isolate dim munging from float extraction
    The dimension flipping is a separate thing, which can be
    done separately thanks to ImgLib2's Views.permute method.

--- src/main/java/net/imagej/tensorflow/Tensors.java
+++ src/main/java/net/imagej/tensorflow/Tensors.java
@@ -3,6 +3,7 @@ package net.imagej.tensorflow;
 import java.nio.FloatBuffer;
 
 import net.imglib2.Cursor;
+import net.imglib2.Dimensions;
 import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.Img;
@@ -12,6 +13,8 @@ import net.imglib2.img.basictypeaccess.array.FloatArray;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.util.Intervals;
+import net.imglib2.view.IntervalView;
+import net.imglib2.view.Views;
 
 import org.tensorflow.Graph;
 import org.tensorflow.Output;
@@ -34,9 +37,9 @@ public final class Tensors {
 	public static <T extends RealType<T>> Tensor tensor(
 		final RandomAccessibleInterval<T> image)
 	{
-		final long[] dims = Intervals.dimensionsAsLongArray(image);
-		final long[] reshapedDims = new long[] { dims[1], dims[0], dims[2] };
-		final float[] value = floatArray(image);
+		final IntervalView<T> imageCXY = Views.permute(Views.permute(image, 2, 1), 0, 1);
+		final long[] reshapedDims = reversedDims(imageCXY);
+		final float[] value = floatArray(imageCXY);
 
 		try (final Graph g = new Graph()) {
 			final GraphBuilder b = new GraphBuilder(g);
@@ -51,27 +54,28 @@ public final class Tensors {
 		}
 	}
 
+	private static long[] reversedDims(final Dimensions image) {
+		final long[] dims = new long[image.numDimensions()];
+		for (int d = 0; d < dims.length; d++) {
+			dims[dims.length - d - 1] = image.dimension(d);
+		}
+		return dims;
+	}
+
 	private static <T extends RealType<T>> float[] floatArray(
 		final RandomAccessibleInterval<T> image)
 	{
 		// TODO we can be way more efficient here...
 		final RandomAccess<T> source = image.randomAccess();
 		final long[] dims = Intervals.dimensionsAsLongArray(image);
-		final long[] reshapedDims = new long[] { dims[1], dims[0], dims[2] };
 
 		final ArrayImg<FloatType, FloatArray> dest = ArrayImgs.floats(
-			reshapedDims);
+			dims);
 		final Cursor<FloatType> destCursor = dest.cursor();
-		for (int y = 0; y < dims[1]; y++) {
-			source.setPosition(y, 1);
-			for (int x = 0; x < dims[0]; x++) {
-				source.setPosition(x, 0);
-				for (int c = 0; c < dims[2]; c++) {
-					destCursor.fwd();
-					source.setPosition(c, 2);
-					destCursor.get().setReal(source.get().getRealDouble());
-				}
-			}
+		while (destCursor.hasNext()) {
+			destCursor.fwd();
+			source.setPosition(destCursor);
+			destCursor.get().setReal(source.get().getRealDouble());
 		}
 		return dest.update(null).getCurrentStorageArray();
 	}
